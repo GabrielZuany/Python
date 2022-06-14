d8e5a0a82903fbd4807fb358273d45fbaea32d43
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-06-14 00:50:13 -0300

    Resolução de questões

--- exercícios/Ex_10-Conversor-Moedas.py
+++ exercícios/Ex_10-Conversor-Moedas.py
@@ -1,2 +1,7 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar -----> considerando 1 real = 3.27 dólares
 
+real = float(input('Digite o valor em reais: '))
+
+dolar = real/3.27
+
+print('O valor em dólares é {:.2f}'.format(dolar))
