092c6fa3fbe25aa87a4162f33f30d29fcf87a977
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-08-18 19:09:27 -0300

    Submit resolution

---
+++ Class/MOD2/class_13-Loop2.py
@@ -0,0 +1,4 @@
+count = 1
+while count < 10:
+    print('Hello, World! -> {}' .format(count))
+    count+=1
---
+++ Exercises/MOD2/Ex_36-GuessNumberV2.py
@@ -0,0 +1,15 @@
+#Build a script that generates a random number between 0 and 10. Then, challenge the user to guess that number. Print the attempts needed by user to guess the number.
+from random import randint
+rand = randint(0,10)
+UserNum = 11
+attempt = -1
+
+while UserNum != rand:
+    UserNum = int(input('Try to guess the number (0-10): '))
+    attempt+=1
+    print('Attempts: {}' .format(attempt+1))
+    
+print('\n')
+print('-='*10)
+print('WINEER!\nNumber: {}\nAttempts: {}' .format(rand, attempt+1))
+print('-='*10)
