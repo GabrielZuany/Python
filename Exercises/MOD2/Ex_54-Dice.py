6d153fa60e91ae294f4a22a3c73210f14076608b
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-08-22 23:02:19 -0300

    Update Ex_54-Dice.py

--- Exercises/MOD2/Ex_54-Dice.py
+++ Exercises/MOD2/Ex_54-Dice.py
@@ -22,27 +22,9 @@ print('*'*40)
 ranking = sorted(players.items(), key = itemgetter(1), reverse=True)
 
 print('*'*15, 'RANKING','*'*15)
+
 for position, value in enumerate(ranking):
     print(f'{position+1} position = {value[0]} -> {value[1]}')
     sleep(0.5)
-print('*'*40)
-
-'''from random import randint
-from time import sleep
-from operator import itemgetter
-
-player_info = dict()
-game_data = list()
-
-for count in range(0, 4):
-    player_info['name'] = (f'Player{count+1}')
-    player_info['dice'] = randint(1, 6)
-    game_data.append(player_info.copy())
-
-print('*'*15, 'PLAYING','*'*15)
-
-for count in range(0, 4):
-    print(game_data[count].values())
-    sleep(0.5)
     
-print('*'*40)'''
+print('*'*40)
