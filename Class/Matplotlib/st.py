60e17e8d144866693942b24ab37a09a730649b0c
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-10-08 23:42:05 -0300

    Start to learn matplotlib.

---
+++ Class/Matplotlib/st.py
@@ -0,0 +1,36 @@
+
+
+'''
+mean() -> Média aritmética dos dados.
+
+fmean() -> Média arimética de ponto flutuante rápida.
+
+geometric_mean() -> Média geométrica dos dados.
+
+harmonic_mean() -> Média harmônica dos dados.
+
+median() -> Mediana (valor do meio) dos dados.
+
+median_low() -> Mediana inferior dos dados.
+
+median_high() -> Mediana superior dos dados.
+
+median_grouped() -> Mediana, ou o 50º percentil dos dados agrupados.
+
+mode() -> Moda (valor mais comum) de dados discretos ou nominais.
+
+multimode() -> List of modes (most common values) of discrete or nominal data.
+
+quantiles() -> Divide os dados em intervalos com probabilidade igual.
+
+---------------
+
+pstdev() -> Desvio padrão populacional dos dados.
+
+pvariance() -> Variância populacional dos dados.
+
+stdev() -> Desvio padrão amostral dos dados.
+
+variance() -> Variância amostral dos dados.
+
+'''
