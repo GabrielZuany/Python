efd28d16f2acebbedb3b039507f37f6bc87bf8d6
GabrielZuany <gabrielzuanyduartevargas@gmail.com>
2022-10-11 19:50:22 -0300

    Upload files and commented the source code

--- Projects/MovieRating_WebScraping/MovieRatingWS/MovieRatingWS/spiders/imdb.py
+++ Projects/MovieRating_WebScraping/MovieRatingWS/MovieRatingWS/spiders/imdb.py
@@ -6,16 +6,24 @@ class ImdbSpider(scrapy.Spider):
     start_urls = ['https://www.imdb.com/chart/boxoffice/?ref_=hm_cht_sm']
 
     def parse(self, response):
+        
+        # Get all movies.
         for mv in response.css('tr'):
+            
+            # Avoid empty data write.
             if mv.css('.titleColumn a::text').get() != None:
                 title = mv.css('.titleColumn a::text').get()
                 weekend = mv.css('.ratingColumn::text').get()
                 gross = mv.css('.secondaryInfo::text').get()
                 weeks = mv.css('.weeksColumn::text').get()
+                
+                
+                # Build a dictionary with the collect data.
                 yield{
                     'title': title.strip(),
                     'weekend': weekend.strip(), 
                     'gross': gross.strip(),
                     'weeks': weeks.strip()
                 }
+                
         pass
