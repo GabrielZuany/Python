bb483c8db683901809e34af31526c4b97a98099b
GabrielZuany <gzuanydev@gmail.com>
2023-01-02 19:26:28 -0300

    Files uploaded

---
+++ DataScience/16_Web_DataScienceApp/src/diretivas_de_compilacao.py
@@ -0,0 +1,21 @@
+TOTAL_VENDA_PRODUTOS_DO_FUNCIONARIO = "Total de venda acumulada"
+QTD_VENDA_PRODUTOS_FUNCIONARIO = 'Numero de Vendas'
+RENDIMENTO_FUNCIONARIO = 'Rendimento comparado ao mes passado (%)'
+PRECO_PRODUTO = 'Valor do Produto'
+ID_FUNCIONARIO = 'ID Funcionario'
+PRODUTO = "Produto"
+MEDIA_VALOR_X_QTD_VENDIDA = 'Media por venda'
+OUTRAS_INFORMACOES = 'Outras Informacoes'
+CAMPO_LOJA = 'Loja'
+ID_LOJA = 'ID LOJA'
+
+PATH_IN = "../"
+PATH_OUT = "../RelatorioLojas/"
+RANKING_FILENAME = '02_RankingLojas.xlsx'
+RELATORIO_FILENAME = "01_RelatorioLojas.xlsx"
+FULL_DF_IN = 'TesteLoja.xlsx'
+
+SHEET_FUNCIONARIOS = 'Funcionarios'
+SHEET_GERAL = 'Geral'
+
+UNNAMED = '^Unnamed'
